18c9438754bfce9463d35e421b6f9e2377ffceb5
katsiarynalashcheuskaya <ek.sharova@icloud.com>
2023-11-05 01:23:18 -0400

    Add NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+    Link,
+    Paper,
+    Box,
+    BottomNavigation,
+    BottomNavigationAction
+} from '@mui/material';
+import {LOGIN} from "App";
+import SchoolIcon from '@mui/icons-material/School';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SpeakerNotesIcon from '@mui/icons-material/SpeakerNotes';
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+
+export default function NavBar() {
+    const [value, setValue] = React.useState(0);
+    const ref = React.useRef(null);
+
+    return (
+        <Box sx={{pb: 7}} ref={ref}>
+            <Paper sx={{position: 'fixed', top: 0, left: 0, right: 0}} elevation={3}>
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue)
+                    }}
+                >
+                    {/* The link to the login page has been added for testing purposes and will be replaced as soon as the main page is ready. */}
+                    <BottomNavigationAction component={Link} to={LOGIN} label="Quizzes" icon={<SchoolIcon/>}/>
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon/>}/>
+                    <BottomNavigationAction label="Notes" icon={<SpeakerNotesIcon/>}/>
+                    <BottomNavigationAction label="Help Center" icon={<HelpRoundedIcon/>}/>
+                </BottomNavigation>
+            </Paper>
+        </Box>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from "@mui/material";
 import {LOGIN, SIGNUP} from "App";
+import NavBar from "components/NavBar";
 
 export default function Home() {
     return (
         <div>
+            <NavBar/>
             <Link href={LOGIN}>Login</Link>
             <Link href={SIGNUP}>Sign up</Link>
         </div>
