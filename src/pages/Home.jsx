443d510457f76b8f4219a7cf0823116a79c16015
katsiarynalashcheuskaya <ek.sharova@icloud.com>
2023-11-14 15:45:42 -0500

    Create header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import {Link} from "@mui/material";
 import {LOGIN, SIGNUP} from "App";
 import NavBar from "components/NavBar";
+import Header from "../components/Header";
+
+const profileSettings = ['Account', 'Logout'];
+const userData={
+    firstName: "Katsiaryna",
+    lastName: "Lashcheuskaya",
+    email: "test@test.com",
+}
 
 export default function Home() {
     return (
         <div>
+            <Header profileSettings={profileSettings} userData={userData}/>
             <NavBar/>
-            <Link href={LOGIN}>Login</Link>
-            <Link href={SIGNUP}>Sign up</Link>
+           {/* <Link href={LOGIN}>Login</Link>
+            <Link href={SIGNUP}>Sign up</Link>*/}
         </div>
     );
 };
