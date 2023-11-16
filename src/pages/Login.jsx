537d0432336b80680ee9cb87e2179db0e8f99bae
David Greenslade <david.greenslade.cohort2211@gmail.com>
2023-11-15 18:22:48 -0800

    Login and Signup pages with functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
     Avatar,
     Button,
@@ -8,23 +9,121 @@ import {
     Box,
     Grid,
     Typography,
+    InputAdornment,
+    IconButton,
     Checkbox,
     FormControlLabel
 } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import {RESET_PASSWORD, SIGNUP} from "App";
+import { Visibility, VisibilityOff} from '@mui/icons-material';
+import {RESET_PASSWORD, SIGNUP, HOME} from "App";
 import customColors from "assets/styles";
 
 export default function Login() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        /*//debugging code for testing receiving data from the form, delete when adding functionality
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        })*/
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [loginData, setLoginData] = React.useState({
+        //username: '', 
+        email: '',
+        password: ''
+    });
+
+    const [errors, setErrors] = React.useState({
+        firstName: null,
+        lastName: null,
+        username: null, 
+        password: null, 
+        email: null
+    });
+
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const navigate = useNavigate();
+
+    //signs user in with credentials
+    const loginUser = async (newUserData) => {
+        const loginData = {
+            //'username': newUserData.username,
+            'email': newUserData.email,
+            'password': newUserData.password,
+            //'quizData' : {}
+        };
+
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                //'Authorization': `Bearer YOUR_TOKEN` 
+            },
+            body: JSON.stringify(loginData),
+            credentials: 'include' 
+        };
+
+        const url = `http://localhost:8000/api/v1/login`; // API endpoint here
+            
+        try {
+            const response = await fetch(url, options);
+            if(!response.ok) {
+                const errorMessage = `Error: ${response.status} - ${response.statusText}`;
+                const responseBody = await response.json(); 
+                console.error(errorMessage, responseBody); 
+                throw new Error(errorMessage);
+            }
+            const data = await response.json();
+            return {
+                success: data.success,
+                message:data.message
+            };
+        } catch (error) {
+            console.error(error.message); // Using console.error for errors.
+            throw error; // Re-throw to be caught in the calling function.
+        }
+    }
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    
+    const handleMouseDownPassword = (event) => {
+    event.preventDefault();
     };
+
+    const handleLoginDataChange = (field) => (e) => {
+        setLoginData(prevData => ({ ...prevData, [field]: e.target.value }));
+    }
+
+    const handleSubmitData = async (event, loginData) => {
+        try {
+            event.preventDefault();
+            setIsLoading(true);
+            const apiStatus = await loginUser(loginData);
+            console.log(apiStatus);
+            setIsLoading(false);
+            setLoginData({
+                //username: '', 
+                email: '',
+                password: ''
+            }); // Resetting the login data to its initial state.
+            //navigate(HOME);
+        } catch (error) {
+            setIsLoading(false); // Ensure loading state is reset even on error.
+            console.error(error.message);
+            // TODO: Provide feedback to the user about the error.
+        }
+    }
+
+    const onSignupRedirect = ()=>{
+        setLoginData({
+           //username: '', 
+            password: '', 
+            email: ''
+        });
+    }
+
+    const onForgotPassword = ()=>{
+        setLoginData({
+            //username: '', 
+            password: '', 
+            email: ''
+        });
+    }
 
     return (
         <>
@@ -58,7 +157,7 @@ export default function Login() {
                         <Typography component="h1" variant="h5">
                             Sign in
                         </Typography>
-                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 1}}>
+                        <Box component="form" noValidate onSubmit={(e) => handleSubmitData(e, loginData)} sx={{mt: 1}}>
                             <TextField
                                 margin="normal"
                                 required
@@ -68,6 +167,7 @@ export default function Login() {
                                 name="email"
                                 autoComplete="email"
                                 autoFocus
+                                onChange={handleLoginDataChange('email')}
                             />
                             <TextField
                                 margin="normal"
@@ -75,9 +175,21 @@ export default function Login() {
                                 fullWidth
                                 name="password"
                                 label="Password"
-                                type="password"
                                 id="password"
                                 autoComplete="current-password"
+                                type={showPassword ? 'text' : 'password'}
+                                onChange={handleLoginDataChange('password')}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
                             />
                             <FormControlLabel
                                 control={<Checkbox value="remember" color="primary"/>}
@@ -99,12 +211,12 @@ export default function Login() {
                             </Button>
                             <Grid container>
                                 <Grid item xs>
-                                    <Link href={RESET_PASSWORD} variant="body2" color="primary.main">
+                                    <Link href={RESET_PASSWORD} onClick={onForgotPassword} variant="body2" color="primary.main">
                                         Forgot password?
                                     </Link>
                                 </Grid>
                                 <Grid item>
-                                    <Link href={SIGNUP} variant="body2" color="primary.main">
+                                    <Link href={SIGNUP} onClick={onSignupRedirect} variant="body2" color="primary.main">
                                         Don't have an account? Sign Up
                                     </Link>
                                 </Grid>
