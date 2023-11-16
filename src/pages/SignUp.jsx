634e4950d21671d567eca0f6cd53e85b841486a1
David Greenslade <david.greenslade.cohort2211@gmail.com>
2023-11-15 18:22:48 -0800

    Login and Signup pages with functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
     Avatar,
     Button,
@@ -7,24 +8,137 @@ import {
     Paper,
     Box,
     Grid,
+    InputAdornment,
+    IconButton,
     Typography
 } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Visibility, VisibilityOff} from '@mui/icons-material';
 import customColors from "assets/styles";
 import {LOGIN} from "App";
 
 export default function SignUp() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        /*//debugging code for testing receiving data from the form, delete when adding functionality
-        const data = new FormData(event.currentTarget);
-         console.log({
-             firstName: data.get('firstName'),
-             lastName: data.get('lastName'),
-             email: data.get('email'),
-             password: data.get('password'),
-         });*/
+
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [userData, setUserData] = React.useState({
+        firstName: '',
+        lastName:'',
+        username: '', 
+        email: '',
+        password: ''
+    });
+
+    const [errors, setErrors] = React.useState({
+        firstName: null,
+        lastName: null,
+        username: null, 
+        password: null, 
+        email: null
+    });
+
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const navigate = useNavigate();
+
+    //updates API with new user registration data
+    const addUser = async (newUserData) => {
+        const registrationData = {
+            'firstname': newUserData.firstName,
+            'lastname': newUserData.lastName,
+            //'username': newUserData.username,
+            'email': newUserData.email,
+            'password': newUserData.password,
+            //'quizData' : {}
+        };
+
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                //'Authorization': `Bearer YOUR_TOKEN` 
+            },
+            body: JSON.stringify(registrationData)
+        };
+
+        const url = `http://localhost:8000/api/v1/signup`; // API endpoint here
+            
+        try {
+            const response = await fetch(url, options);
+            if(!response.ok) {
+                const errorMessage = `Error: ${response.status} - ${response.statusText}`;
+                const responseBody = await response.json(); 
+                console.error(errorMessage, responseBody); 
+                throw new Error(errorMessage);
+            }
+            const data = await response.json();
+            return {
+                id: data._id,
+                firstname: data.firstname,
+                lastname: data.lastname
+            };
+        } catch (error) {
+            console.error(error.message); // Using console.error for errors.
+            throw error; // Re-throw to be caught in the calling function.
+        }
+    }
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    
+    const handleMouseDownPassword = (event) => {
+    event.preventDefault();
     };
+
+    const handleUserDataChange = (field) => (e) => {
+        setUserData(prevData => ({ ...prevData, [field]: e.target.value }));
+    }
+
+    const handleSubmitData = async (event, userData) => {
+        try {
+            event.preventDefault();
+            setIsLoading(true);
+            const userInfo = await addUser(userData);
+            console.log(userInfo);
+            setIsLoading(false);
+            setUserData({
+                firstName: '',
+                lastName:'',
+                username: '', 
+                password: '', 
+                email: ''
+            }); // Resetting the user data to its initial state.
+            navigate(LOGIN);
+        } catch (error) {
+            setIsLoading(false); // Ensure loading state is reset even on error.
+            console.error(error.message);
+            // TODO: Provide feedback to the user about the error.
+        }
+    }
+
+
+    const onLoginRedirect = ()=>{
+        alert(`Let's go to the Login Page!`)
+        setUserData({
+            firstName: '',
+            lastName:'',
+            username: '', 
+            password: '', 
+            email: ''
+        });
+    }
+
+    const onForgotPassword = (data)=>{
+        alert(`lets rest the password for ${data.username}!`)
+        setUserData({
+            firstName: '',
+            lastName:'',
+            username: '', 
+            password: '', 
+            email: ''
+        });
+    }
+
+    console.log(userData);
 
     return (
         <>
@@ -59,7 +173,7 @@ export default function SignUp() {
                         <Typography component="h1" variant="h5">
                             Sign up
                         </Typography>
-                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
+                        <Box component="form" noValidate onSubmit={(e) => handleSubmitData(e, userData)} sx={{mt: 3}}>
                             <Grid container spacing={2}>
                                 <Grid item xs={12} sm={6}>
                                     <TextField
@@ -70,6 +184,7 @@ export default function SignUp() {
                                         id="firstName"
                                         label="First Name"
                                         autoFocus
+                                        onChange={handleUserDataChange('firstName')}
                                     />
                                 </Grid>
                                 <Grid item xs={12} sm={6}>
@@ -80,6 +195,7 @@ export default function SignUp() {
                                         label="Last Name"
                                         name="lastName"
                                         autoComplete="family-name"
+                                        onChange={handleUserDataChange('lastName')}
                                     />
                                 </Grid>
                                 <Grid item xs={12}>
@@ -90,6 +206,7 @@ export default function SignUp() {
                                         label="Email Address"
                                         name="email"
                                         autoComplete="email"
+                                        onChange={handleUserDataChange('email')}
                                     />
                                 </Grid>
                                 <Grid item xs={12}>
@@ -98,9 +215,21 @@ export default function SignUp() {
                                         fullWidth
                                         name="password"
                                         label="Password"
-                                        type="password"
                                         id="password"
                                         autoComplete="new-password"
+                                        type={showPassword ? 'text' : 'password'}
+                                        onChange={handleUserDataChange('password')}
+                                        endAdornment={
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                >
+                                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        }
                                     />
                                 </Grid>
                             </Grid>
@@ -108,15 +237,17 @@ export default function SignUp() {
                                 type="submit"
                                 fullWidth
                                 variant="contained"
-                                sx={[{
-                                    '&:hover': {backgroundColor: customColors.greyDark}
-                                },
+                                disabled={isLoading} // Disable button when loading
+                                sx={[
+                                    {
+                                        '&:hover': {backgroundColor: customColors.greyDark}
+                                    },
                                     {
                                         mt: 3, mb: 2, backgroundColor: customColors.blackLight,
                                     },
                                 ]}
                             >
-                                Sign Up
+                                {isLoading ? <CircularProgress size={24} /> : 'Sign Up'}
                             </Button>
                             <Grid container justifyContent="flex-end">
                                 <Grid item>
