acf2aa81c9b6ad57adec45e2e8ed397d024c93b4
evaw277 <evaweisneck@gmail.com>
2023-11-30 17:06:36 -0500

    Added notes to filters // Prettier fixes
    added some notes from Katherine about filter functions, added spaces to make code more readable. Prettier edited some things when saved.

--- src/components/filterState.js
+++ src/components/filterState.js
@@ -1,36 +1,40 @@
-import {useCallback, useEffect, useState} from "react";
-import {useLocation} from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const useFilterState = () => {
-    const [activeFilters, setActiveFilters] = useState({
-        levels: [],
-        categories: [],
-        labels: [],
+  const [activeFilters, setActiveFilters] = useState({
+    levels: [],
+    categories: [],
+    labels: [],
+  });
+
+  //add new filter to activeFilters
+  const setActiveFilter = useCallback((type, filter) => {
+    setActiveFilters((prevFilters) => ({
+      ...prevFilters,
+      [type]: toggleFilter(prevFilters[type], filter),
+    }));
+  }, []);
+
+  //remove unchecked filter from activeFilters
+  const toggleFilter = (prevFilters, filter) =>
+    filter
+      ? prevFilters.includes(filter)
+        ? prevFilters.filter((f) => f !== filter)
+        : [...prevFilters, filter]
+      : [];
+
+  const location = useLocation();
+  useEffect(() => {
+    // Reset activeFilters when the route changes
+    setActiveFilters({
+      levels: [],
+      categories: [],
+      labels: [],
     });
+  }, [location.pathname]);
 
-    const setActiveFilter = useCallback((type, filter) => {
-        setActiveFilters(prevFilters => ({
-            ...prevFilters,
-            [type]: toggleFilter(prevFilters[type], filter),
-        }));
-    }, []);
-
-    const toggleFilter = (prevFilters, filter) => (
-        filter
-            ? (prevFilters.includes(filter) ? prevFilters.filter(f => f !== filter) : [...prevFilters, filter])
-            : []
-    );
-    const location = useLocation();
-    useEffect(() => {
-        // Reset activeFilters when the route changes
-        setActiveFilters({
-            levels: [],
-            categories: [],
-            labels: [],
-        });
-    }, [location.pathname]);
-
-    return {activeFilters, setActiveFilter};
+  return { activeFilters, setActiveFilter };
 };
 
 export default useFilterState;
