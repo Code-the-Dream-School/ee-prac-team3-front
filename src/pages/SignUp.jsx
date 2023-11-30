3c8aad95a2590fd5c9cabc623246c7d9709984a4
David Greenslade <david.greenslade.cohort2211@gmail.com>
2023-11-29 22:54:13 -0800

    backend communication fix

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -244,7 +244,7 @@ export default function SignUp() {
               </Button>
               <Grid container justifyContent="flex-end">
                 <Grid item>
-                  <Link href = {LOGIN} onClick = {onRedirect} variant="body2">
+                  <Link href={LOGIN} onClick={onRedirect} variant="body2">
                     Already have an account? Sign in
                   </Link>
                 </Grid>
