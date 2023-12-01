3d2b426185c430a9eec4705df4cf698796ec055c
evaw277 <evaweisneck@gmail.com>
2023-12-01 17:23:18 -0500

    added Link to card

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   Box,
   Card,
@@ -22,142 +23,147 @@ const Quiz = ({ quiz, activeFilters, getProgressForQuiz }) => {
 
   return (
     <Grid item key={quiz.id} xs={12} sm={6} md={4}>
-      <Card
-        elevation={1}
-        sx={{
-          height: "100%",
-          display: "flex",
-          padding: "20px 30px",
-          minWidth: "270px",
-          transition: "transform 0.2s",
-          "&:hover": {
-            transform: "scale(1.05)",
-            backgroundColor: customColors.hover,
-          },
-        }}
-      >
-        <Grid
-          container
-          direction="column"
-          justifyContent="space-between"
-          alignItems="flex-start"
+      <Link to="/">
+        <Card
+          elevation={1}
+          sx={{
+            height: "100%",
+            display: "flex",
+            padding: "20px 30px",
+            minWidth: "270px",
+            transition: "transform 0.2s",
+            "&:hover": {
+              transform: "scale(1.05)",
+              backgroundColor: customColors.hover,
+            },
+          }}
         >
-          <CardContent
-            sx={{
-              display: "flex",
-              padding: "0 !important",
-              justifyContent: "space-between",
-              "& > div": {
-                flex: "1 1 0",
-              },
-              height: "100%",
-              width: "100%",
-            }}
+          <Grid
+            container
+            direction="column"
+            justifyContent="space-between"
+            alignItems="flex-start"
           >
-            {/* this is the content of the left side inside each card */}
-            <Box
+            <CardContent
               sx={{
                 display: "flex",
-                flexDirection: "column",
+                padding: "0 !important",
                 justifyContent: "space-between",
+                "& > div": {
+                  flex: "1 1 0",
+                },
+                height: "100%",
+                width: "100%",
               }}
             >
+              {/* this is the content of the left side inside each card */}
               <Box
                 sx={{
                   display: "flex",
-                  flexDirection: "row",
-                  flexWrap: "wrap",
-                  gap: "9px",
-                  marginRight: "10px",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
                 }}
               >
-                <Labels
-                  labels={quiz.labels}
-                  isFilterActive={isFilterActive}
-                ></Labels>
-                <Chip
+                <Box
                   sx={{
-                    borderRadius: "4px",
-                    fontWeight: "bold",
-                    backgroundColor: isFilterActive("levels", quiz.level)
-                      ? customColors.blueMedium
-                      : customColors.grey,
-                    color: isFilterActive("levels", quiz.level)
-                      ? customColors.grey
-                      : customColors.blueMedium,
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    gap: "9px",
+                    marginRight: "10px",
                   }}
-                  size={isSmallScreen ? "medium" : "small"}
-                  label={quiz.level}
-                />
-                <Chip
-                  sx={{
-                    borderRadius: "4px",
-                    fontWeight: "bold",
-                    backgroundColor: isFilterActive("categories", quiz.category)
-                      ? customColors.blueMedium
-                      : customColors.grey,
-                    color: isFilterActive("categories", quiz.category)
-                      ? customColors.grey
-                      : customColors.blueMedium,
-                  }}
-                  size={isSmallScreen ? "medium" : "small"}
-                  label={quiz.category}
-                />
-              </Box>
-              <Box>
-                <Typography
-                  sx={{ marginTop: "10px", color: customColors.greyMedium }}
                 >
-                  {progress.attemptsCount
-                    ? `Attempts: ${progress.attemptsCount}`
-                    : "Not Passed"}
-                </Typography>
+                  <Labels
+                    labels={quiz.labels}
+                    isFilterActive={isFilterActive}
+                  ></Labels>
+                  <Chip
+                    sx={{
+                      borderRadius: "4px",
+                      fontWeight: "bold",
+                      backgroundColor: isFilterActive("levels", quiz.level)
+                        ? customColors.blueMedium
+                        : customColors.grey,
+                      color: isFilterActive("levels", quiz.level)
+                        ? customColors.grey
+                        : customColors.blueMedium,
+                    }}
+                    size={isSmallScreen ? "medium" : "small"}
+                    label={quiz.level}
+                  />
+                  <Chip
+                    sx={{
+                      borderRadius: "4px",
+                      fontWeight: "bold",
+                      backgroundColor: isFilterActive(
+                        "categories",
+                        quiz.category
+                      )
+                        ? customColors.blueMedium
+                        : customColors.grey,
+                      color: isFilterActive("categories", quiz.category)
+                        ? customColors.grey
+                        : customColors.blueMedium,
+                    }}
+                    size={isSmallScreen ? "medium" : "small"}
+                    label={quiz.category}
+                  />
+                </Box>
+                <Box>
+                  <Typography
+                    sx={{ marginTop: "10px", color: customColors.greyMedium }}
+                  >
+                    {progress.attemptsCount
+                      ? `Attempts: ${progress.attemptsCount}`
+                      : "Not Passed"}
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
 
-            {/* this is the content of the right side inside each card */}
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
-            >
+              {/* this is the content of the right side inside each card */}
               <Box
-                marginTop="25px"
                 sx={{
                   display: "flex",
                   flexDirection: "column",
                   justifyContent: "center",
-                  alignItems: "center",
                 }}
               >
-                <CardMedia
-                  width="100%"
-                  component="div"
+                <Box
+                  marginTop="25px"
                   sx={{
-                    width: "63px",
-                    height: "62px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
                   }}
-                  image={quiz.image}
-                />
-                <Progress progress={progress.progress} />
-              </Box>
-              <Box sx={{ textAlign: "center" }}>
-                <Typography
-                  variant={"h6"}
-                  component="h2"
-                  marginTop="40px"
-                  fontWeight="bold"
-                  lineHeight="22px"
                 >
-                  {quiz.title}
-                </Typography>
+                  <CardMedia
+                    width="100%"
+                    component="div"
+                    sx={{
+                      width: "63px",
+                      height: "62px",
+                    }}
+                    image={quiz.image}
+                  />
+                  <Progress progress={progress.progress} />
+                </Box>
+                <Box sx={{ textAlign: "center" }}>
+                  <Typography
+                    variant={"h6"}
+                    component="h2"
+                    marginTop="40px"
+                    fontWeight="bold"
+                    lineHeight="22px"
+                  >
+                    {quiz.title}
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
-          </CardContent>
-        </Grid>
-      </Card>
+            </CardContent>
+          </Grid>
+        </Card>
+      </Link>
     </Grid>
   );
 };
