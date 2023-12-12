32ee53fca2a67ca2283e7880ee719c607be14531
katsiarynalashcheuskaya <ek.sharova@icloud.com>
2023-12-12 12:26:16 -0500

    Make some style changes

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Box, Container, Typography } from '@mui/material';
+import customColors from "../assets/styles";
 
 export default function Error() {
   return (
     <Container
       sx={{
-        minHeight: '88vh',
-        pt: 6,
-        pb: 2,
+          minHeight: '85vh',
+          backgroundColor: customColors.backgroundLight,
+          maxWidth: 'none !important',
+          pt: 6,
+          pb: 2,
       }}
     >
       <Box>
