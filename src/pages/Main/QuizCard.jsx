21d8771beb67092ff15501512b98a2c233a3c52b
DavidGslade86 <david.greenslade.cohort2211@gmail.com>
2023-12-18 00:25:35 -0800

    bugfix with user progress reset

--- src/pages/Main/QuizCard.jsx
+++ src/pages/Main/QuizCard.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import useQuiz from '../../quiz/useQuiz';
+import useAuth from '../../auth/useAuth';
 import {
   Box,
   Card,
@@ -37,6 +39,9 @@ const QuizCard = ({
   const [anchorEl, setAnchorEl] = useState(null);
   const [cardClickable, setCardClickable] = useState(true);
   const [isFavorite, setIsFavorite] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { quizzes, setQuizzes } = useQuiz();
+  const { auth } = useAuth();
 
   useEffect(() => {
     favoritesIds && setIsFavorite(favoritesIds.includes(quiz.id));
@@ -95,200 +100,213 @@ const QuizCard = ({
 
   return (
     <Grid item key={quiz.id} xs={12} sm={6} md={4}>
-      <Card
-        elevation={1}
-        onClick={(event) => {
-          // Only navigate when clicking on the non-interactive parts of the card
-          if (!event.target.closest('.menu-icon') && cardClickable) {
-            window.location.href = `${QUIZ}/${quiz.id}`;
-          }
-        }}
-        sx={{
-          height: '100%',
-          display: 'flex',
-          padding: '20px 30px',
-          minWidth: '270px',
-          position: 'relative',
-          transition: 'transform 0.2s',
-          cursor: 'pointer',
-          '&:hover': {
-            transform: 'scale(1.05)',
-            /*backgroundColor: customColors.hover,*/
-          },
-        }}
-      >
-        <Grid
-          container
-          direction="column"
-          justifyContent="space-between"
-          alignItems="flex-start"
+      {loading ? (
+        <Typography> Resetting progres...</Typography>
+      ) : (
+        <Card
+          elevation={1}
+          onClick={(event) => {
+            // Only navigate when clicking on the non-interactive parts of the card
+            if (!event.target.closest('.menu-icon') && cardClickable) {
+              window.location.href = `${QUIZ}/${quiz.id}`;
+            }
+          }}
+          sx={{
+            height: '100%',
+            display: 'flex',
+            padding: '20px 30px',
+            minWidth: '270px',
+            position: 'relative',
+            transition: 'transform 0.2s',
+            cursor: 'pointer',
+            '&:hover': {
+              transform: 'scale(1.05)',
+              /*backgroundColor: customColors.hover,*/
+            },
+          }}
         >
-          <CardContent
-            sx={{
-              display: 'flex',
-              padding: '0 !important',
-              justifyContent: 'space-between',
-              '& > div': {
-                flex: '1 1 0',
-              },
-              height: '100%',
-              width: '100%',
-            }}
+          <Grid
+            container
+            direction="column"
+            justifyContent="space-between"
+            alignItems="flex-start"
           >
-            {/* this is the content of the left side inside each card */}
-            <Box
+            <CardContent
               sx={{
                 display: 'flex',
-                flexDirection: 'column',
+                padding: '0 !important',
                 justifyContent: 'space-between',
+                '& > div': {
+                  flex: '1 1 0',
+                },
+                height: '100%',
+                width: '100%',
               }}
             >
+              {/* this is the content of the left side inside each card */}
               <Box
                 sx={{
                   display: 'flex',
-                  flexDirection: 'row',
-                  flexWrap: 'wrap',
-                  gap: '9px',
-                  marginRight: '10px',
+                  flexDirection: 'column',
+                  justifyContent: 'space-between',
                 }}
               >
-                <Labels
-                  labels={quiz.labels}
-                  isFilterActive={isFilterActive}
-                ></Labels>
-                <Chip
+                <Box
                   sx={{
-                    borderRadius: '4px',
-                    fontWeight: 'bold',
-                    backgroundColor: isFilterActive('levels', quiz.level)
-                      ? customColors.blueMedium
-                      : customColors.grey,
-                    color: isFilterActive('levels', quiz.level)
-                      ? customColors.grey
-                      : customColors.blueMedium,
-                  }}
-                  size={isSmallScreen ? 'medium' : 'small'}
-                  label={quiz.level}
-                />
-                <Chip
-                  sx={{
-                    borderRadius: '4px',
-                    fontWeight: 'bold',
-                    backgroundColor: isFilterActive('categories', quiz.category)
-                      ? customColors.blueMedium
-                      : customColors.grey,
-                    color: isFilterActive('categories', quiz.category)
-                      ? customColors.grey
-                      : customColors.blueMedium,
-                  }}
-                  size={isSmallScreen ? 'medium' : 'small'}
-                  label={quiz.category}
-                />
-              </Box>
-              <Box>
-                <Typography
-                  sx={{
-                    marginTop: '10px',
-                    color: customColors.greyDark,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                    gap: '9px',
+                    marginRight: '10px',
                   }}
                 >
-                  {bestScore ? `Score: ${bestScore}%` : ''}
-                </Typography>
-                <Typography
-                  sx={{
-                    color: customColors.greyDark,
-                  }}
-                >
-                  {bestScore === 100
-                    ? 'Passed'
-                    : attemptsCount
-                      ? `Attempts: ${attemptsCount}`
-                      : 'Not Passed'}
-                </Typography>
+                  <Labels
+                    labels={quiz.labels}
+                    isFilterActive={isFilterActive}
+                  ></Labels>
+                  <Chip
+                    sx={{
+                      borderRadius: '4px',
+                      fontWeight: 'bold',
+                      backgroundColor: isFilterActive('levels', quiz.level)
+                        ? customColors.blueMedium
+                        : customColors.grey,
+                      color: isFilterActive('levels', quiz.level)
+                        ? customColors.grey
+                        : customColors.blueMedium,
+                    }}
+                    size={isSmallScreen ? 'medium' : 'small'}
+                    label={quiz.level}
+                  />
+                  <Chip
+                    sx={{
+                      borderRadius: '4px',
+                      fontWeight: 'bold',
+                      backgroundColor: isFilterActive(
+                        'categories',
+                        quiz.category
+                      )
+                        ? customColors.blueMedium
+                        : customColors.grey,
+                      color: isFilterActive('categories', quiz.category)
+                        ? customColors.grey
+                        : customColors.blueMedium,
+                    }}
+                    size={isSmallScreen ? 'medium' : 'small'}
+                    label={quiz.category}
+                  />
+                </Box>
+                <Box>
+                  <Typography
+                    sx={{
+                      marginTop: '10px',
+                      color: customColors.greyDark,
+                    }}
+                  >
+                    {bestScore ? `Score: ${bestScore}%` : ''}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: customColors.greyDark,
+                    }}
+                  >
+                    {bestScore === 100
+                      ? 'Passed'
+                      : attemptsCount
+                        ? `Attempts: ${attemptsCount}`
+                        : 'Not Passed'}
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
-            {/* this is the content of the right side inside each card */}
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-              }}
-            >
+              {/* this is the content of the right side inside each card */}
               <Box
-                marginTop="25px"
                 sx={{
                   display: 'flex',
                   flexDirection: 'column',
                   justifyContent: 'center',
-                  alignItems: 'center',
                 }}
               >
-                <CardMedia
-                  width="100%"
-                  component="div"
+                <Box
+                  marginTop="25px"
                   sx={{
-                    width: '54px',
-                    height: '60px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}
-                  image={quiz.image}
-                />
-                <Progress progress={bestScore} />
+                >
+                  <CardMedia
+                    width="100%"
+                    component="div"
+                    sx={{
+                      width: '54px',
+                      height: '60px',
+                    }}
+                    image={quiz.image}
+                  />
+                  <Progress progress={bestScore} />
+                </Box>
+                <Box sx={{ textAlign: 'center' }}>
+                  <Typography
+                    variant={'h6'}
+                    component="h2"
+                    marginTop="40px"
+                    fontWeight="bold"
+                    lineHeight="22px"
+                  >
+                    {highlightSearchTerm(quiz.title, searchValue)}
+                  </Typography>
+                </Box>
               </Box>
-              <Box sx={{ textAlign: 'center' }}>
-                <Typography
-                  variant={'h6'}
-                  component="h2"
-                  marginTop="40px"
-                  fontWeight="bold"
-                  lineHeight="22px"
+              <Box sx={{ position: 'absolute', right: 3, top: 3 }}>
+                <IconButton
+                  aria-controls="simple-menu"
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                  className="menu-icon"
                 >
-                  {highlightSearchTerm(quiz.title, searchValue)}
-                </Typography>
+                  <MoreVertIcon />
+                </IconButton>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      if (isFavorite) {
+                        handleRemoveFavorite(quiz.id);
+                      } else {
+                        handleAddFavorite(quiz.id);
+                      }
+                      handleClose();
+                    }}
+                  >
+                    {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+                  </MenuItem>
+                  <MenuItem
+                    onClick={(event) => {
+                      event.stopPropagation();
+                      handleResetProgress(
+                        quiz,
+                        auth,
+                        quizzes,
+                        setQuizzes,
+                        setLoading
+                      );
+                    }}
+                  >
+                    Reset progress
+                  </MenuItem>
+                </Menu>
               </Box>
-            </Box>
-            <Box sx={{ position: 'absolute', right: 3, top: 3 }}>
-              <IconButton
-                aria-controls="simple-menu"
-                aria-haspopup="true"
-                onClick={handleClick}
-                className="menu-icon"
-              >
-                <MoreVertIcon />
-              </IconButton>
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem
-                  onClick={(event) => {
-                    event.stopPropagation();
-                    if (isFavorite) {
-                      handleRemoveFavorite(quiz.id);
-                    } else {
-                      handleAddFavorite(quiz.id);
-                    }
-                    handleClose();
-                  }}
-                >
-                  {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
-                </MenuItem>
-                <MenuItem
-                  onClick={(event) => {
-                    event.stopPropagation();
-                    handleResetProgress();
-                  }}
-                >
-                  Reset progress
-                </MenuItem>
-              </Menu>
-            </Box>
-          </CardContent>
-        </Grid>
-      </Card>
+            </CardContent>
+          </Grid>
+        </Card>
+      )}
     </Grid>
   );
 };
