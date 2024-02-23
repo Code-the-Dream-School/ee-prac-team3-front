9aa5adc2ea65f4167e1c202ee14284b3f4349894
katsiarynalashcheuskaya <ek.sharova@icloud.com>
2024-02-22 22:37:17 -0500

    Format code

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export let BASE_URL;
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Set production API URL
   BASE_URL = 'https://ee-prac-team3-back.onrender.com/api/v1';
 } else {
