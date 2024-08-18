8336299ae1edabb93ee068d1bab082b906ab4a0e
katsiarynalashcheuskaya <ek.sharova@icloud.com>
2024-08-17 23:52:34 -0400

    WIP: fixing cors problem

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ import {
 } from './functions/exportFunctions';
 import Notes from 'pages/Notes/notesPage';
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000/api/v1/';
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000/api/v1/';
 
 const PATH = {
   HOME: '/home',
